2a0f6340c4bd1853fa6703fb5dd2c20763827eff
11334906 <ricgommar@alu.edu.gva.es>
2023-11-08 10:21:11 +0100

    Prueba2

---
+++ EntornoDesarrollo/Ejercicios/src/AdiosMundo.java
@@ -0,0 +1,7 @@
+//Escribirn un programa que escriba por pantalla "Adiós mundo"
+public class AdiosMundo {
+	public static void main (String[] args){
+		System.out.println ("Adiós mundo");
+		
+	}	
+}
