e081ba1e4c936cd8f98cbd3557cb5d63f4be2eb1
11334906 <ricgommar@alu.edu.gva.es>
2023-11-22 08:28:34 +0100

    Miercoles 22

--- EntornoDesarrollo/Ejercicios/src/ActividadDepuracion.java
+++ EntornoDesarrollo/Ejercicios/src/ActividadDepuracion.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class ActividadDepuracion {
+    final Scanner tec = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        double radio, longitud, area;
+        System.out.println("Introduce radio de la circunferencia:");
+        radio = tec.nextDouble();
+        longitud = 2 * Math.PI * radio;
+        area = Math.PI * Math.pow(radio, Math.PI);
+        System.out.println("Longitud de la circunferencia -> " + longitud);
+        System.out.println("Area de la circunferencia -> " + area);
     }
 }
